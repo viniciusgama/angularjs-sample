09e5f46b1e824060d90a53b9697a32a7be734ec3
Vinicius Gama <viniciussgama@msn.com>
2012-07-28 18:46:57 -0300

    Moving the remove button to the top right of the address

--- addresses.js
+++ addresses.js
@@ -1,7 +1,15 @@
 function AddressesController($scope){
-	$scope.addresses = [{address:'Rua Vicente da Fontoura', city:'Porto Alegre', zipcode:'31110130', country:'Brazil', state:'RS'}];
+	$scope.addresses = [
+		{
+			address:'Rua Vicente da Fontoura',
+			city:'Porto Alegre',
+			zipcode:'31110130',
+			country:'Brazil',
+			state:'RS'
+		}
+	];
 
 	$scope.addNewAddress = function(){
-		$scope.addresses.push({address:'', city:'', zipcode:'', country:'', state:''});
+		$scope.addresses.push({});
 	};
 }
